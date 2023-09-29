fd87276eaa84565832c21304507440d194b84d9b
Sarcasl <126521612+Sarcasl@users.noreply.github.com>
2023-09-28 17:55:03 -0700

    Update

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 function Sort({ sort, setSort }) {
     return (
         <div className='dropdown'>
+            <h4>Sort</h4>
             <select value={sort} onChange={(e) => { setSort(`?sort=${e.target.value}`) }}>
                 <option></option>
                 <option>asc</option>
